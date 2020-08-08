c84861311b6e5a3d8716fa85a5a898968fa0b1c3
Kelin Field <kfield111@hotmail.com>
2020-08-08 05:12:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -90,15 +90,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash = {
-	:yukihiro_matsumoto = {
-		:known_for => "Ruby",
-		:languages => ["LISP", "C"]
-	}
-}
-
+programmer_hash[:yukihiro_matsumoto][:known_for = "Ruby",	:languages = ["LISP", "C"]]
 
 end
+
+
 
 def changing_alan
 	programmer_hash =
