f20502d7304c9de4df659f380dde8972fe76c505
Kelin Field <kfield111@hotmail.com>
2020-08-08 04:53:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,7 +63,7 @@ def dennis_ritchies_language
         }
      }
 
-		puts programmer_hash[:dennis_ritchies][:languages[0]]
+		puts programmer_hash[:dennis_ritchies][:languages]
 end
 
 def adding_matz
